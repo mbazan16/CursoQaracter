db895663eccff112a31b4e4931682152fe111b1b
Maria Bazan <mariabazan@gmail.com>
2023-02-05 14:14:48 +0100

    Proyecto de practicas de excepciones

---
+++ pexceptions/src/com/proyecto/common/Constantes.java
@@ -0,0 +1,10 @@
+package com.proyecto.common;
+
+public class Constantes {
+	//Paso 1. Constante de cadena int "TAMANIO_MAX" con valor 25
+	
+	//Paso 2. Constante de cadena int "TAMANIO_MIN" con valor 2
+	
+	
+ 
+}

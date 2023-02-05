1557105b62ed7ba0fd296f34c19fed145d787e14
Maria Bazan <mariabazan@gmail.com>
2023-02-05 14:10:31 +0100

    Proyecto de practicas de soluciones

---
+++ pexceptions/src/com/proyecto/Test.java
@@ -0,0 +1,36 @@
+package com.proyecto;
+
+import java.util.Scanner;
+
+import com.proyecto.common.exception.NotAllowedWordsException;
+import com.proyecto.common.exception.StringNotValidException;
+import com.proyecto.common.exception.StringTooLongException;
+import com.proyecto.common.exception.StringTooShortException;
+import com.proyecto.common.util.ValidationString;
+
+public class Test {
+
+	// Todo el código excepto las declaraciones debe estar en un bloque try-catch
+	public static void main(String[] args)  {
+		// Declarar las variables necesarias		
+		
+			
+			// Paso 1. Realizar un bucle infinito, que únicamente se pare
+			// cuando el código lance una excepcion de cadena nula o vacía o 
+			// de palabra no permitida introducida
+				// Paso 2. Pedir una cadena por consola al usuario
+				
+				// Paso 3. Validar si la cadena es correcta con los valores por defecto
+				// Si no es correcta mostrar al usuario el mensaje adecuado por cada caso
+				// Implementar otro bloque try/catch anidado que solo capture las excepciones
+		        // de tamaño.
+						
+						// Paso 3.1. Se muestra un mensaje de cadena válida
+		//Paso 4. Cuando se recojan todas las excepciones (de ambos bloques try/cach)
+		//mostrar un mensaje adecuado a la  excepción recogida				
+			
+											
+	}			
+	
+
+}

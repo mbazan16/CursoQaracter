f987a2a5d059da6ff58647215910adecc8e0ae31
Maria Bazan <mariabazan@gmail.com>
2023-02-05 14:15:57 +0100

    Proyecto de soluciones de excepciones

---
+++ sexceptions/src/com/proyecto/common/Constantes.java
@@ -0,0 +1,10 @@
+package com.proyecto.common;
+
+public class Constantes {
+	//Paso 1. Constante de int "TAMANIO_MAX" con valor 25
+	public static final int TAMANIO_MAX=25;
+	//Paso 2. Constante de  int "TAMANIO_MIN" con valor 2
+	public static final int TAMANIO_MIN=2;
+	
+ 
+}

09c811cf844d85ae88f8dd3583d91885aa426465
rubenPortos9 <ruben.portos@qaracter.es>
2023-02-06 20:02:21 +0100

    Commit pExceptionRuben

---
+++ pExceptionRuben/src/com/proyecto/common/exception/NotAllowedWordsException.java
@@ -0,0 +1,23 @@
+package com.proyecto.common.exception;
+
+import com.proyecto.common.NotAllowedWord;
+
+//Paso 1. Identificar esta clase commo hija de StringNotValidException
+public class NotAllowedWordsException extends StringNotValidException {
+
+	// Paso 2. Indicar un atributo tipo NotAllowedWord llamado "word"
+	private NotAllowedWord word;
+	
+	//Paso 3. Implementar un constructor que tenga los parametros  necesarios 
+	// para invocar al constructor padre y asignar/establecer el valor de word.
+	public NotAllowedWordsException(String c, String msg, NotAllowedWord word) {
+		super(c, msg);
+		this.word = word;
+	}
+
+	//Paso 4. Implementar el método get para devolver el valor de word
+	public NotAllowedWord getWord() {
+		return word;
+	}
+	
+}

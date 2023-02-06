db95b098f27a2c69e473ab4597061c39a6704431
xabner233 <abner.oliveira@mark2market.com.br>
2023-02-06 20:02:15 +0100

    .

---
+++ pexeptionsAbner/src/com/proyecto/common/exception/CodeErrors.java
@@ -0,0 +1,16 @@
+package com.proyecto.common.exception;
+
+public class CodeErrors {
+	//Paso 1. Constante de cadena String "NO_STRING" con valor ER_STR001	
+	public String NO_STRING = "ER_STR001";
+	
+	//Paso 2. Constante de cadena String "STRING_TOO_LONG" con valor ER_STR002	
+	public String STRING_TOO_LONG = "ER_STR002";
+	
+	//Paso 3. Constante de cadena String "STRING_TOO_SHORT" con valor ER_STR003	
+	public String STRING_TOO_SHORT = "ER_STR003";
+	
+	//Paso 4. Constante de cadena String "WORD_NOT_ALLOWED_FOUND" con valor ER_STR004	
+	public String WORD_NOT_ALLOWED_FOUND ="ER_STR004";
+	
+}

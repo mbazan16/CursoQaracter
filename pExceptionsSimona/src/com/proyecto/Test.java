6e5571ac2ac13e3ec7750cc6f6bb12a67037c026
SimonaAlexandraVisan <122809090+SimonaAlexandraVisan@users.noreply.github.com>
2023-02-07 12:51:30 +0100

    Practica finalizada

--- pExceptionsSimona/src/com/proyecto/Test.java
+++ pExceptionsSimona/src/com/proyecto/Test.java
@@ -11,35 +11,46 @@ import com.proyecto.common.util.ValidationString;
 public class Test {
 
 	// Todo el código excepto las declaraciones debe estar en un bloque try-catch
-	public static void main(String[] args)  {
+	@SuppressWarnings({ "static-access"})
+	public static void main(String[] args) throws StringNotValidException{
 		// Declarar las variables necesarias
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Por favor, introduzca una palabra.");
 		String cadena = sc.next();
 
-		ValidationString val = new ValidationString();
-		try {
-			val.validLenght(cadena);
-		} catch (StringNotValidException e) {
-			System.out.println(e);
+		while (true) {
+			try {
+				try {
+					ValidationString.validLenght(cadena);
+					System.out.println("Cadena introducida correcta.");
+				} catch (StringTooShortException e) {
+					System.out.println("Cadena demasiado corta. Tiene " + e.getTamanyo() + " caracteres.");
+				} catch (StringTooLongException e1) {
+					System.out.println("Cadena demasiado larga. Tiene " + e1.getTamanyo() + " caracteres.");
+				}
+			} catch (NotAllowedWordsException e2) {
+				System.out.println("Cadena con palabras prohibidas. Usa " + e2.getWord());
+				throw e2;
+			} catch (StringNotValidException e3) {
+				System.out.println("Cadena nula o vacia");
+				throw e3;
+			}
 		}
-			
-			// Paso 1. Realizar un bucle infinito, que únicamente se pare
-			// cuando el código lance una excepcion de cadena nula o vacía o 
-			// de palabra no permitida introducida
-				// Paso 2. Pedir una cadena por consola al usuario
-				
-				// Paso 3. Validar si la cadena es correcta con los valores por defecto
-				// Si no es correcta mostrar al usuario el mensaje adecuado por cada caso
-				// Implementar otro bloque try/catch anidado que solo capture las excepciones
-		        // de tamaño.
-						
-						// Paso 3.1. Se muestra un mensaje de cadena válida
-		//Paso 4. Cuando se recojan todas las excepciones (de ambos bloques try/cach)
-		//mostrar un mensaje adecuado a la  excepción recogida				
-			
-											
-	}			
-	
+
+		// Paso 1. Realizar un bucle infinito, que únicamente se pare
+		// cuando el código lance una excepcion de cadena nula o vacía o
+		// de palabra no permitida introducida
+		// Paso 2. Pedir una cadena por consola al usuario
+
+		// Paso 3. Validar si la cadena es correcta con los valores por defecto
+		// Si no es correcta mostrar al usuario el mensaje adecuado por cada caso
+		// Implementar otro bloque try/catch anidado que solo capture las excepciones
+		// de tamaño.
+
+		// Paso 3.1. Se muestra un mensaje de cadena válida
+		// Paso 4. Cuando se recojan todas las excepciones (de ambos bloques try/cach)
+		// mostrar un mensaje adecuado a la excepción recogida
+
+	}
 
 }
--- pExceptionsSimona/src/com/proyecto/common/util/ValidationString.java
+++ pExceptionsSimona/src/com/proyecto/common/util/ValidationString.java
@@ -12,6 +12,7 @@ import com.proyecto.common.exception.StringNotValidException;
 import com.proyecto.common.exception.StringTooLongException;
 import com.proyecto.common.exception.StringTooShortException;
 
+@SuppressWarnings("unused")
 public class ValidationString {
 
 	// Paso 1. Implementar un método estático "validLenght"que valide
@@ -23,24 +24,7 @@ public class ValidationString {
 		// Paso 1.3 Validar si es menor que el tamaño minimo (Constantes)
 		
 		// Paso 1.4 Validar si no contiene palabras no permitidas;
-	private static void validador(String cadena, int max, int min) throws StringNotValidException {
-		if (cadena.isEmpty() || cadena == null) throw new StringNotValidException("NO_STRING", "Cadena vacía o nula.");
-		if(cadena.length() > max) throw new StringTooLongException("STRING_TOO_LONG", "Cadena demasiado larga", cadena.length());
-		if(cadena.length() < min) throw new StringTooShortException("STRING_TOO_SHORT", "Cadena demasiado corta", cadena.length());
-		notAllowedValidador(cadena);
-	}
-	
-	private static void notAllowedValidador(String cadena) throws NotAllowedWordsException {
-		if(cadena.toUpperCase().contains(NotAllowedWord.DELETE.name())) throw new NotAllowedWordsException("WORD_NOT_ALLOWED_FOUND", "Palabra prohibida usada", NotAllowedWord.DELETE);
-		if(cadena.toUpperCase().contains(NotAllowedWord.DROP.name())) throw new NotAllowedWordsException("WORD_NOT_ALLOWED_FOUND", "Palabra prohibida usada", NotAllowedWord.DROP);
-		if(cadena.toUpperCase().contains(NotAllowedWord.INSERT.name())) throw new NotAllowedWordsException("WORD_NOT_ALLOWED_FOUND", "Palabra prohibida usada", NotAllowedWord.INSERT);
-		if(cadena.toUpperCase().contains(NotAllowedWord.SELECT.name())) throw new NotAllowedWordsException("WORD_NOT_ALLOWED_FOUND", "Palabra prohibida usada", NotAllowedWord.SELECT);
-		if(cadena.toUpperCase().contains(NotAllowedWord.TRUNCATE.name())) throw new NotAllowedWordsException("WORD_NOT_ALLOWED_FOUND", "Palabra prohibida usada", NotAllowedWord.TRUNCATE);
-		if(cadena.toUpperCase().contains(NotAllowedWord.UPDATE.name())) throw new NotAllowedWordsException("WORD_NOT_ALLOWED_FOUND", "Palabra prohibida usada", NotAllowedWord.UPDATE);
-		
-	}
-	
-	
+
 	public static void validLenght(String cadena) throws StringNotValidException  {
 		validador(cadena, new Constantes().TAMANIO_MAX, new Constantes().TAMANIO_MIN);
 	}
@@ -59,6 +43,15 @@ public class ValidationString {
 	}
 
 	// Paso 3. Si alguna funcionalidad se repitiese extraerla en un método privado
-	
+	private static void validador(String cadena, int max, int min) throws StringNotValidException {
+		if (cadena.isEmpty() || cadena == null) throw new StringNotValidException("NO_STRING", "Cadena vacía o nula.");
+		if(cadena.length() > max) throw new StringTooLongException("STRING_TOO_LONG", "Cadena demasiado larga", cadena.length());
+		if(cadena.length() < min) throw new StringTooShortException("STRING_TOO_SHORT", "Cadena demasiado corta", cadena.length());
+		
+		for(NotAllowedWord word: NotAllowedWord.values()){
+			if(cadena.toUpperCase().contains(word.name())) throw new NotAllowedWordsException("WORD_NOT_ALLOWED_FOUND", "Palabra prohibida usada", word);
+			
+		}
+	}
 
 }

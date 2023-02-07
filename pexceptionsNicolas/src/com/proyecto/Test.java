41b60e59a8c8acf6f3e8fec5f01c4a14592f0561
nicocespede <cespede.alannicolas.42984@gmail.com>
2023-02-07 17:51:12 +0100

    pexceptions terminado

--- pexceptionsNicolas/src/com/proyecto/Test.java
+++ pexceptionsNicolas/src/com/proyecto/Test.java
@@ -13,27 +13,43 @@ public class Test {
 	// Todo el c�digo excepto las declaraciones debe estar en un bloque try-catch
 	public static void main(String[] args) {
 		// Declarar las variables necesarias
+		String cadena;
 
-		// Paso 1. Realizar un bucle infinito, que �nicamente se pare
-		// cuando el c�digo lance una excepcion de cadena nula o vac�a o
-		// de palabra no permitida introducida
-		
-		
-		// Paso 2. Pedir una cadena por consola al usuario
-		
+		try (Scanner scanner = new Scanner(System.in)) {
+			// Paso 1. Realizar un bucle infinito, que �nicamente se pare
+			// cuando el c�digo lance una excepcion de cadena nula o vac�a o
+			// de palabra no permitida introducida
 
-		// Paso 3. Validar si la cadena es correcta con los valores por defecto
-		// Si no es correcta mostrar al usuario el mensaje adecuado por cada caso
-		// Implementar otro bloque try/catch anidado que solo capture las excepciones
-		// de tama�o.
-		
+			while (true) {
+				try {
+					// Paso 2. Pedir una cadena por consola al usuario
+					System.out.println("Ingrese una cadena.");
+					cadena = scanner.nextLine();
 
-		// Paso 3.1. Se muestra un mensaje de cadena v�lida
-		
-		
-		// Paso 4. Cuando se recojan todas las excepciones (de ambos bloques try/cach)
-		// mostrar un mensaje adecuado a la excepci�n recogida
+					// Paso 3. Validar si la cadena es correcta con los valores por defecto
+					// Si no es correcta mostrar al usuario el mensaje adecuado por cada caso
+					// Implementar otro bloque try/catch anidado que solo capture las excepciones
+					// de tama�o.
 
+					ValidationString.validLength(cadena);
+
+					// Paso 3.1. Se muestra un mensaje de cadena v�lida
+					System.out.println("La cadena es válida");
+
+					// Paso 4. Cuando se recojan todas las excepciones (de ambos bloques try/cach)
+					// mostrar un mensaje adecuado a la excepci�n recogida
+
+				} catch (StringTooLongException e) {
+					System.out.println(e.getMessage());
+				} catch (StringTooShortException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+		} catch (NotAllowedWordsException e) {
+			System.out.println(e.getMessage());
+		} catch (StringNotValidException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 }
--- pexceptionsNicolas/src/com/proyecto/common/util/ValidationString.java
+++ pexceptionsNicolas/src/com/proyecto/common/util/ValidationString.java
@@ -1,7 +1,9 @@
 package com.proyecto.common.util;
 
 import com.proyecto.common.Constantes;
+import com.proyecto.common.NotAllowedWord;
 import com.proyecto.common.exception.CodeErrors;
+import com.proyecto.common.exception.NotAllowedWordsException;
 import com.proyecto.common.exception.StringNotValidException;
 import com.proyecto.common.exception.StringTooLongException;
 import com.proyecto.common.exception.StringTooShortException;
@@ -12,39 +14,75 @@ public class ValidationString {
 	// si una cadena tiene mas tama�ano de 25 posiciones y menos de 1 posici�n
 	// El m�todo tiene que devolver void, utilizar excepciones, declarar las
 	// necesarias
-	public static void validLength(String s) throws
-	/*
-	 * StringTooLongException, StringTooShortException,
-	 */
-	StringNotValidException {
+	public static void validLength(String s) throws StringNotValidException {
 		// Paso 1.1. Validar si la cadena no es nula y no esta vac�a
 
-		if (s == null || s.isEmpty())
-			throw new StringNotValidException(CodeErrors.NO_STRING, "La cadena es null o está vacía");
+		validarBasico(s);
 
 		// Paso 1.2 Validar si es mayor que el tama�o maximo (Constantes)
 
-		if (s.length() > Constantes.TAMANIO_MAX)
-			throw new StringTooLongException(CodeErrors.STRING_TOO_LONG, "La cadena es muy larga", s.length());
+		validarMaximaLongitud(s, Constantes.TAMANIO_MAX);
 
 		// Paso 1.3 Validar si es menor que el tama�o minimo (Constantes)
 
-		if (s.length() < Constantes.TAMANIO_MIN)
-			throw new StringTooShortException(CodeErrors.STRING_TOO_SHORT, "La cadena es muy corta", s.length());
+		validarMinimaLongitud(s, Constantes.TAMANIO_MIN);
 
 		// Paso 1.4 Validar si no contiene palabras no permitidas;
-		
-		
+
+		validarPalabrasNoPermitidas(s);
 	}
 
 	// Paso 2: Sobrecargar el m�todo anterior para que realize las
 	// validaciones con dos valores m�ximo y m�nimo pasados por par�metros
 
-	// Paso 1.1. Validar si la cadena no es nula y no esta vac�a
-	// Paso 1.2 Validar si es mayor que el tama�o maximo (Constantes)
-	// Paso 1.3 Validar si es menor que el tama�o minimo (Constantes)
-	// Paso 1.4 Validar si no contiene palabras no permitidas;
+	public static void validLength(String s, int maxLength, int minLength) throws StringNotValidException {
+		// Paso 1.1. Validar si la cadena no es nula y no esta vac�a
+
+		validarBasico(s);
+
+		// Paso 1.2 Validar si es mayor que el tama�o maximo (Constantes)
+
+		validarMaximaLongitud(s, maxLength);
+
+		// Paso 1.3 Validar si es menor que el tama�o minimo (Constantes)
+
+		validarMinimaLongitud(s, minLength);
+
+		// Paso 1.4 Validar si no contiene palabras no permitidas;
+
+		validarPalabrasNoPermitidas(s);
+	}
 
 	// Paso 3. Si alguna funcionalidad se repitiese extraerla en un m�todo privado
 
+	private static void validarBasico(String s) throws StringNotValidException {
+		if (s == null)
+			throw new StringNotValidException(CodeErrors.NO_STRING, "La cadena es null");
+
+		if (s.isEmpty())
+			throw new StringNotValidException(CodeErrors.NO_STRING, "La cadena está vacía");
+	}
+
+	private static void validarMaximaLongitud(String s, int maxLength) throws StringTooLongException {
+		if (s.length() > maxLength)
+			throw new StringTooLongException(CodeErrors.STRING_TOO_LONG, "La cadena es muy larga", s.length());
+	}
+
+	private static void validarMinimaLongitud(String s, int minLength) throws StringTooShortException {
+		if (s.length() < minLength)
+			throw new StringTooShortException(CodeErrors.STRING_TOO_SHORT, "La cadena es muy corta", s.length());
+	}
+
+	/**
+	 * 
+	 * @param s
+	 * @throws NotAllowedWordsException
+	 */
+	private static void validarPalabrasNoPermitidas(String s) throws NotAllowedWordsException {
+		s = s.toUpperCase();
+		for (NotAllowedWord value : NotAllowedWord.values())
+			if (s.contains(value.toString()))
+				throw new NotAllowedWordsException(CodeErrors.WORD_NOT_ALLOWED_FOUND,
+						"La cadena contiene palabras no permitidas", value);
+	}
 }

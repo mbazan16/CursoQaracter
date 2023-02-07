54bf1103485b04561580da68c45e4761bdc9789f
xabner233 <abner.oliveira@mark2market.com.br>
2023-02-07 16:14:29 +0100

    .

--- pexeptionsAbner/src/com/proyecto/common/exception/StringNotValidException.java
+++ pexeptionsAbner/src/com/proyecto/common/exception/StringNotValidException.java
@@ -3,10 +3,18 @@ package com.proyecto.common.exception;
 public class StringNotValidException  {
 	
 	//Paso 1.Indicar atributo protegido de tipo String llamado "code"
+	protected String code;
 
-			
+	
 	//Paso 2. Implementar un contructor, que tenga por parametro un valor para code
 	// y un mensaje para el constructor del padre
+	
+	public StringNotValidException(String code) {
+		super();
+		this.code = code;
+	}
+		
+	
 
 	
 	
--- pexeptionsAbner/src/com/proyecto/common/exception/StringTooLongException.java
+++ pexeptionsAbner/src/com/proyecto/common/exception/StringTooLongException.java
@@ -6,10 +6,21 @@ public class StringTooLongException extends StringNotValidException {
 	
 	//Paso 2. Indicar un atributo inmutable tipo int llamado "tamanyo"
 	
+	private int tamanho;
+
+	
+	
 	//Paso 3. Implementar el constructor adecuado para asignar valor a todos
 	// los atributos de esta clase incluidos los heredados
+	public StringTooLongException(String code, int tamanho) {
+		super(code);
+		this.tamanho = tamanho;
+	}
+	
+	
 	
 	//Paso 4. Implementar un metodo para conseguir el valor del atributo "tamanyo"
+	
 	
 	
 	
